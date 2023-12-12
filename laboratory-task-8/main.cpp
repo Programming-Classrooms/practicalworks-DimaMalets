7e499bf915b826fe633f22d242409caa49f335e2
DimaMalets <dmalec017@gmail.com>
2023-12-12 22:37:16 +0300

    Залил 8 лабу

---
+++ laboratory-task-8/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Header.h"
+
+//6. Строка состоит из слов, разделённых знаками препинания. За одинпросмотр символов строки найти все самые длинные слова и занести их в
+//новую строку. Слова в новой строке должны разделяться ровно однимпробелом.
+
+int main() {
+	std::string line;
+	size_t counter = 0;
+	size_t reserveCounter = 0;
+	std::string newLine;
+	std::string delim = " /.,:;'  ";
+	inputAndCheckLine(line);
+	searchWords(line, delim, newLine, counter, reserveCounter);
+	std::cout << newLine;
+
+}
