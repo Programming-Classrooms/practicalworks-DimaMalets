4de5d524e53c7ae437510ba08d2c56579cad4d99
DimaMalets <dmalec017@gmail.com>
2023-12-13 23:43:07 +0300

    2 лаба 3 задание

---
+++ laboratory-task-2/lab2-3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include<iomanip>
+
+//3. добавл€ет слева и справа в записи числа цифру, повтор€ющуюс€
+//максимальное число раз;
+
+void inputNumber(int32_t& number) {
+    std::cout << "Input natural number:\n";
+    std::cin >> number;
+    if (number <= 0)
+    {
+        std::cout << "Not a natural!!!\n";
+        inputNumber(number);
+    }
+}
+
+int main()
+{
+    int32_t temp_first;
+    int32_t temp_second;
+    int32_t number;
+    int32_t theDesiredFigure;
+    int32_t counterRepetitions = 0;
+    int32_t numberRepetitions = 0;
+    uint16_t degree = 1;
+    inputNumber(number);
+    temp_second = number;
+    temp_first = number;
+    for (size_t i = 0; i <= 9; i++)
+    {
+        temp_first = number;
+        counterRepetitions = 0;
+        while (temp_first != 0)
+        {
+            if (temp_first % 10 == i)
+            {
+                counterRepetitions++;
+            }
+            temp_first /= 10;
+        }
+        if (counterRepetitions >= numberRepetitions)
+        {
+            theDesiredFigure = i;
+            numberRepetitions = counterRepetitions;
+        }
+    }
+    while (number != 0) {
+        number /= 10;
+        degree++;
+    }
+        std::cout << "New number = " << std::setprecision(10) << theDesiredFigure * pow(10, degree) + temp_second * 10 + theDesiredFigure;
+        return 0;
+
+}
