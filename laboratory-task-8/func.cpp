768c748671d47643a3205f0ce15256c0461ddc5c
DimaMalets <dmalec017@gmail.com>
2023-12-24 17:45:03 +0300

    Переименовал файл,отступы,ifndef

--- laboratory-task-8/func.cpp
+++ laboratory-task-8/func.cpp
@@ -1,42 +1,38 @@
-#include <iostream>
-#include <string>
-#include "Header.h"
-
-void inputAndCheckLine(std::string& line) {
-	std::cout << "Enter the line \n";
-	getline(std::cin, line);
-	if (line.empty() == true) {
-		std::cout << "Line is empty";
-	}
-
+#include <iostream> 
+#include <string> 
+#include "Header.hpp" 
+ 
+void inputAndCheckLine(std::string& line) 
+{ 
+ std::cout << "Enter the line \n"; 
+ getline(std::cin, line); 
+ if (line.empty() == true) { 
+  std::cout << "Line is empty"; 
+ } 
+} 
+ 
+void createNewLine(std::string word, std::string& newLine, size_t& counter, size_t& reserveCounter) 
+{ 
+    counter = word.length(); 
+    if (counter >=  reserveCounter) { 
+        newLine = newLine + word + ' '; 
+        reserveCounter = counter; 
+    } 
+} 
+ 
+void searchWords(std::string line, std::string delim,std::string& newLine, size_t& counter, size_t& reserveCounter)  
+{ 
+    std::string::size_type begInd; 
+    std::string word; 
+    begInd = line.find_first_not_of(delim); 
+    while (begInd != std::string::npos) { 
+        std::string::size_type endInd; 
+        endInd = line.find_first_of(delim, begInd); 
+        if (endInd == std::string::npos) { 
+            endInd = line.length(); 
+        } 
+        word = line.substr(begInd, endInd - begInd); 
+        createNewLine(word, newLine, counter, reserveCounter); 
+        begInd = line.find_first_not_of(delim, endInd); 
+    } 
 }
-
-void createNewLine(std::string word, std::string& newLine, size_t& counter, size_t& reserveCounter) {
-    counter = word.length();
-    if (counter >=  reserveCounter) {
-        newLine = newLine + word + ' ';
-    }
-    reserveCounter = counter;
-
-}
-
-void searchWords(std::string line, std::string delim,std::string& newLine, size_t& counter, size_t& reserveCounter) {
-    std::string::size_type begInd;
-    std::string word;
-    begInd = line.find_first_not_of(delim);
-    while (begInd != std::string::npos)
-    {
-        std::string::size_type endInd;
-        endInd = line.find_first_of(delim, begInd);
-        if (endInd == std::string::npos)
-        {
-            endInd = line.length();
-        }
-        word = line.substr(begInd, endInd - begInd);
-        createNewLine(word, newLine, counter, reserveCounter);
-        begInd = line.find_first_not_of(delim, endInd);
-    }
-
-}
-
-
--- laboratory-task-8/main.cpp
+++ laboratory-task-8/main.cpp
@@ -1,17 +1,21 @@
-#include <iostream>
-#include "Header.h"
-
-//6. Строка состоит из слов, разделённых знаками препинания. За одинпросмотр символов строки найти все самые длинные слова и занести их в
-//новую строку. Слова в новой строке должны разделяться ровно однимпробелом.
-
-int main() {
-	std::string line;
-	size_t counter = 0;
-	size_t reserveCounter = 0;
-	std::string newLine;
-	std::string delim = " /.,:;'  ";
-	inputAndCheckLine(line);
-	searchWords(line, delim, newLine, counter, reserveCounter);
-	std::cout << newLine;
-
+/* 
+ 6. Строка состоит из слов, разделённых знаками препинания.За один 
+ просмотр символов строки найти все самые длинные слова и занести их в 
+ новую строку.Слова в новой строке должны разделяться ровно одним 
+ пробелом. 
+*/ 
+ 
+ 
+#include <iostream> 
+#include "Header.hpp" 
+ 
+int main() { 
+ std::string line; 
+ size_t counter = 0; 
+ size_t reserveCounter = 0; 
+ std::string newLine; 
+ std::string delim = " /.,:;'  "; 
+ inputAndCheckLine(line); 
+ searchWords(line, delim, newLine, counter, reserveCounter); 
+ std::cout << newLine; 
 }
