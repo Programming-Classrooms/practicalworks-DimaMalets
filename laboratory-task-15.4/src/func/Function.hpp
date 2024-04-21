7663d07b2153c0974c0a367911ae37d5fe6db0de
DimaMalets <dmalec017@gmail.com>
2024-04-21 20:51:47 +0300

    Добавил 4 задание

---
+++ laboratory-task-15.4/src/func/Function.hpp
@@ -0,0 +1,31 @@
+#ifndef FUNCTION_HPP
+#define FUNCTION_HPP
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+/*
+     Заполнение матрицы случайными числами в заданном диапазоне 
+*/
+void fillSize(int16_t &, int16_t &);
+
+
+/*
+     Заполнение матрицы случайными числами в заданном диапазоне 
+*/
+void fillMatrix(std::vector<std::vector<int>>&, int16_t, int16_t);
+
+/* 
+   Удалить строку и столбец, на пересечении которых стоит максимальный
+   элемент матрицы 
+*/
+void deleteMax(std::vector<std::vector<int>>&, int16_t, int16_t);
+
+/* 
+    Упорядочить строки матрицы по возрастанию сумм элементов строк 
+*/
+void  newSort(std::vector<std::vector<int>>&, int16_t, int16_t);
+
+#endif // Function.hpp
