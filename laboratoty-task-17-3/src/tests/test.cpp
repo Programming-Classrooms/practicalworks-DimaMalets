17f3d124611f182addf15fbc5d6301c611fbb433
DimaMalets <dmalec017@gmail.com>
2024-05-13 23:14:11 +0300

    Добавил 3 задание

---
+++ laboratoty-task-17-3/src/tests/test.cpp
@@ -0,0 +1,28 @@
+#include "../funcs/Funcs.hpp"
+#include <gtest/gtest.h> 
+#include <sstream> 
+#include <fstream> 
+
+// Тестирование функции readFile
+TEST(ReadFileTest, BasicTest) {
+    std::multimap<std::string, int16_t> myMap;
+    std ::ifstream in ("src/files/test.txt");
+    readFile(myMap, in);
+    EXPECT_EQ(myMap.count("hello"), 3);
+    EXPECT_EQ(myMap.count("world"), 2);
+}
+
+// Тестирование функции checkFile
+TEST(CheckFileTest, BasicTest) {
+    std::ifstream goodFile("src/files/good_file.txt");
+    std::ifstream emptyFile("src/files/empty_file.txt");
+    std::ifstream badFile("src.files/bad_file.txt");
+    EXPECT_NO_THROW(checkFile(goodFile));
+    EXPECT_THROW(checkFile(emptyFile), std::runtime_error);
+    EXPECT_THROW(checkFile(badFile), std::invalid_argument);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
