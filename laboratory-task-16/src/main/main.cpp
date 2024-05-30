3c8ee8cf54027efd5da3590d82731c977415fd7f
DimaMalets <dmalec017@gmail.com>
2024-05-30 23:20:50 +0300

    добавил main

--- laboratory-task-16/src/main/main.cpp
+++ laboratory-task-16/src/main/main.cpp
@@ -5,22 +5,33 @@ int main()
 {
     try
     {
-        TDeque<CarUnderReraip> a;
-        CarUnderReraip first;
-        first.manualFill();
-        CarUnderReraip second("bmw",1000,"test",100);
+        TDeque<CarUnderReraip> carDeque;
+        
+        CarUnderReraip car1("Toyota", 1500.0, "Engine failure", 5);
+        CarUnderReraip car2("Honda", 1200.0, "Transmission issues", 3);
+        CarUnderReraip car3("BMW", 2500.0, "Suspension repair", 7);
+        CarUnderReraip car4("Audi", 1800.0, "Brake issues", 4);
 
-        a.InsRear(first);
-        a.InsFront(second);
+        carDeque.InsFront(car1);
+        carDeque.InsRear(car2);
+        carDeque.InsFront(car3);
+        carDeque.InsRear(car4);
 
-        std::cout << "\n";
-        a.exportInStream(std::cout);
+        carDeque.DelFront();
+        carDeque.DelRear();
 
-        TDeque<CarUnderReraip> b;
-        b = a;
+        std::cout << "Элемент с индексом 0:\n" << carDeque.GetByIndex(0) << "\n\n";
+        std::cout << "Элемент с индексом 1:\n" << carDeque.GetByIndex(1) << "\n\n";
 
-        std::cout << "\n" << b[1].Info.getCarBrand();
+        carDeque.SetByIndex(CarUnderReraip("Tesla", 3000.0, "НОРМАЛДАКИЧ", 8), 0);
 
+        carDeque.BrowseWithModification([](CarUnderReraip& car) {
+        car.setRepairPrice(car.getrepairPrice() * 0.9);});
+
+        carDeque.BrowseWithoutModification([](const CarUnderReraip& car) {
+        std::cout << car << "\n";});
+
+        
         return 0;
     }
     catch(std::invalid_argument e)
